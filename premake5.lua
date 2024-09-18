d8ff499f38ab954ee75acf6fe0a8a8a3f554b36c
L3b1n <binleo07@icloud.com>
2024-09-18 11:36:14 +0300

    Add support to generate visual studio projects via premake

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "RayTracing"
+	startproject "RayTracing"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "RayTracing"
