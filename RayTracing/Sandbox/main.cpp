b6c960e074c42b7b59b38fd2f5f0e85b90611b7f
Leo Bintsarouski <binleo07@icloud.com>
2024-09-18 15:29:53 +0300

    Add basic test for the renderer class

--- RayTracing/Sandbox/main.cpp
+++ RayTracing/Sandbox/main.cpp
@@ -1,18 +1,57 @@
+#include "Core/Image.h"
+#include "Core/Timer.h"
 #include "Core/Application.h"
 
-#include "Core/Image.h"
+#include "Renderer/Renderer.h"
+
+bool g_ApplicationRunning = true;
 
 class ExampleLayer : public RayTracing::Layer
 {
 public:
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth  = (uint32_t)ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = (uint32_t)ImGui::GetContentRegionAvail().y;
+
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight()});
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
 	}
+
+	void Render()
+	{
+		RayTracing::Timer timer;
+
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	float m_LastRenderTime = 0.0f;
+
+	uint32_t m_ViewportWidth = 0;
+	uint32_t m_ViewportHeight = 0;
+
+	RayTracing::Renderer m_Renderer;
 };
 
 int main(int argc, char** argv)
@@ -33,5 +72,11 @@ int main(int argc, char** argv)
 				ImGui::EndMenu();
 			}
 		});
+
+	while (g_ApplicationRunning)
+	{
+		app->Run();
+		delete app;
+	}
 	return 0;
 }
